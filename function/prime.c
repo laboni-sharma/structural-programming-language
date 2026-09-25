3b6813a8fcc4ee27e137e4c11b0f4473390bb506
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:24:34 +0600

    prime

---
+++ function/prime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    if (n < 2)
+        return 0;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (isPrime(n))
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
