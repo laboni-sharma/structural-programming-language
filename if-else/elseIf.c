b6bc0d9754a047e953a67967972037f281c764d7
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:14:50 +0600

    else if

---
+++ if-else/elseIf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int time = 20;
+    if (time < 12)
+    {
+        printf("good morning");
+    }
+    else if (time < 18)
+    {
+        printf("good day");
+    }
+    else
+    {
+        printf("good evening");
+    }
+    return 0;
+}
