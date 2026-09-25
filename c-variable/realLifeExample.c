294cd07db15ba8316a39d384094c066abdda6fb8
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:39:30 +0600

    real life

---
+++ c-variable/realLifeExample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int studentid = 15;
+    int studentage = 23;
+    float studentfee = 70.45;
+    char studentgrade = 'b';
+    printf("students id : %d\n", studentid);
+    printf("students age : %d\n", studentage);
+    printf("students fee : %g\n", studentfee);
+    printf("students grade : %d\n", studentgrade);
+    return 0;
+}
