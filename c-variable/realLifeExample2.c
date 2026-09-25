7262f544d3815d86e44e4fb0c89d80d335e8c640
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:40:26 +0600

    real life example 2

---
+++ c-variable/realLifeExample2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int length = 4;
+    int width = 6;
+    int area;
+    area = length*width;
+    printf("length is: %d\n", length);
+    printf("width is : %d\n", width);
+    printf("the area of rectangle is : %g\n",area);
+    return 0;
+}
