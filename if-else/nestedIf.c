7985bb855dbdd6101012e39ba0879014a07a6eb9
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:17:14 +0600

    nested if

---
+++ if-else/nestedIf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x = 13;
+    int y = 23;
+    if (x > 10)
+    {
+        peintf("x is greater tham 10\n");
+
+        if (y < 20)
+        {
+            printf("y is greater than 20");
+        }
+    }
+
+    return 0;
+}
