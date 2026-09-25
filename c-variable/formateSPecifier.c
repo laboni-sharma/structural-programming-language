8626a57c77d180a8109c4e0ce5e84034533851f1
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:36:25 +0600

    formate specifier

---
+++ c-variable/formateSPecifier.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num = 15;        // integer
+    float number = 5.99; // float
+    char letter = 'a';   // character
+    printf("%d\n", num);
+    printf("%f\n", number);
+    printf("%c\n", letter);
+    return 0;
+}
