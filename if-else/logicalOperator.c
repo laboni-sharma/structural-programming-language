30c14c9980776f2d92bcc8b9d73703f26df049ec
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:16:42 +0600

    logical operator

---
+++ if-else/logicalOperator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 100;
+    int b = 34;
+    int c = 890;
+    if (a > b && c > a)
+    {
+        printf("both conditions are true\n");
+    }
+    return 0;
+}
