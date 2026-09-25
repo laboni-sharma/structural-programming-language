f9b956e2a42511e9e7572661df8c57de647fb63b
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-25 12:50:08 +0600

    data type

---
+++ c-dataType/daraType.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num = 12;
+    float number = 4.5;
+    char letter = 'c';
+    printf("%d\n", num);
+    printf("%f\n", number);
+    printf("%c\n", letter);
+    return 0;
+}
